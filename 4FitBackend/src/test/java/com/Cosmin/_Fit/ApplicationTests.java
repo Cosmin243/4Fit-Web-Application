c86dcea8fe62e58724ea59a8a75aa29e7e014484
Leonte SS. Cosmin Florin <83459209+Cosmin243@users.noreply.github.com>
2026-02-16 22:51:47 +0200

    first commit
    setup

---
+++ 4FitBackend/src/test/java/com/Cosmin/_Fit/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cosmin._Fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
